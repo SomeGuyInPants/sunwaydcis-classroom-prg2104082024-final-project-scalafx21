dafaa3abce33daf7a997c5371803f1436f6da111
Jonas Ho <jonas.ho@Live.com>
2024-12-08 22:01:28 +0800

    removed unused imports in testRoom.scala

--- src/main/scala/testRoom.scala
+++ src/main/scala/testRoom.scala
@@ -2,15 +2,12 @@ import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.scene.input.KeyCode
-import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
 import scalafx.Includes.jfxKeyEvent2sfx
 import scalafx.animation.AnimationTimer
 import scalafx.scene.SceneIncludes.jfxScene2sfx
 import scala.collection.mutable
-import scalafx.scene.text.{Font, Text}
-import scalafx.scene.media.AudioClip
-import java.nio.file.Paths
+
 
 
 trait Hit :
